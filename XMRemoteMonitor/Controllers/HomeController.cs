1acd15ae64172bce3f33faba16045e579eb72a43
John Calder <manukautech@users.noreply.github.com>
2019-01-27 23:18:43 +1300

    Add files via upload
    Support recording a placeholder image as an all-server-side process by sending an options flag. This means that the client code no longer needs to download, store and provide placeholder image data.

--- XMRemoteMonitor/Controllers/HomeController.cs
+++ XMRemoteMonitor/Controllers/HomeController.cs
@@ -54,12 +54,17 @@ namespace XMRemoteMonitor.Controllers
         [HttpGet]
         public string GetSettings()
         {
+            //2019-01-27 JPC the sending of placeholder data is now only for backward compatibility
+            //method ImagePost below now handles this task all on the server in response to an options flag
             List<string> imgBase64 = new List<string>();
             imgBase64.Add(System.IO.File.ReadAllText(_env.WebRootPath + "\\res\\PlaceholderBase64.txt"));
+
             ResponseAJAX response = new ResponseAJAX()
             {
-                categoryid = 20, method = "GetSettings",
-                issuccess = true, debug = _debug,
+                categoryid = 20,
+                method = "GetSettings",
+                issuccess = true,
+                debug = _debug,
                 xdata = imgBase64
             };
             //2019-01-16 apply our new convention of "signal" as the keyword for a package of metadata and data in json string format
@@ -70,7 +75,7 @@ namespace XMRemoteMonitor.Controllers
         }
 
         [HttpPost]
-        public string ImagePost(string CameraNumber, string AccessKey, string ImageBase64)
+        public string ImagePost(string CameraNumber, string AccessKey, string ImageBase64, string options = "base64")
         {
             ResponseAJAX response = new ResponseAJAX();
             response.categoryid = 21;
@@ -89,39 +94,56 @@ namespace XMRemoteMonitor.Controllers
             string folderName = DateTime.Now.ToString("yyyy-MM-dd") + "\\" + CameraNumber;
             System.IO.Directory.CreateDirectory(webRoot + "\\modet\\" + folderName);
             string fileWithLocation = "modet\\" + folderName + "\\" + XDayTime() + ".jpg";
-            var filePath = Path.Combine(webRoot, fileWithLocation);
+            string filePath = Path.Combine(webRoot, fileWithLocation);
 
-            string tracking = "Begin Base64 conversion to binary.";
-            
-            //var textFilePath = Path.Combine(webRoot, "images\\MotionDetected\\" + folderName + "\\log.txt");
-            try {
-                //20181220 JPC experiment of saving the base64 version of the image
-                //Works with web interfaces but not for local apps like Image Viewer or Explorer icons.
-                //System.IO.File.AppendText(filePath).Write(ImageBase64);
-
-                var bytes = Convert.FromBase64String(ImageBase64);
-                if (bytes.Length < 1000)
+            if(options == "placeholder")
+            {
+                //2019-01-27 JPC handle placeholder by sending an options message rather than the full data
+                try
+                {
+                    //copy file "Placeholder.jpg" from its "res" folder to the recorded sequence of images
+                    string placeholderPath = Path.Combine(webRoot, "res\\Placeholder.jpg");
+                    System.IO.File.Copy(placeholderPath, filePath);
+                    response.message = fileWithLocation.Replace("\\", "/");
+                    response.issuccess = true;
+                }
+                catch (Exception e)
                 {
                     response.issuccess = false;
-                    response.message = "INCOMPLETE data.";
-                    string jsonIncomplete = JsonConvert.SerializeObject(response);
-                    return jsonIncomplete;
+                    response.message = "ERROR: On saving 'Placeholder' image " + e.Message + ".";
                 }
-                tracking = "Image.Load(bytes) ";
-                var img = Image.Load(bytes);
-
-                tracking = "img.SaveAsJpeg ";
-                using (var imageFile = new FileStream(filePath, FileMode.Create))
-                {
-                    img.SaveAsJpeg(imageFile);
-                }
-                response.message = fileWithLocation.Replace("\\", "/");
-                response.issuccess = true;
             }
-            catch (Exception e)
+            else
             {
-                response.issuccess = false;
-                response.message = "ERROR: " + tracking + " " + e.Message + ".";
+                //main process, recording image from client camera
+                string tracking = "Begin Base64 conversion to binary.";
+                //var textFilePath = Path.Combine(webRoot, "images\\MotionDetected\\" + folderName + "\\log.txt");
+                try
+                {
+                    var bytes = Convert.FromBase64String(ImageBase64);
+                    if (bytes.Length < 1000)
+                    {
+                        response.issuccess = false;
+                        response.message = "INCOMPLETE data.";
+                        string jsonIncomplete = JsonConvert.SerializeObject(response);
+                        return jsonIncomplete;
+                    }
+                    tracking = "Image.Load(bytes) ";
+                    var img = Image.Load(bytes);
+
+                    tracking = "img.SaveAsJpeg ";
+                    using (var imageFile = new FileStream(filePath, FileMode.Create))
+                    {
+                        img.SaveAsJpeg(imageFile);
+                    }
+                    response.message = fileWithLocation.Replace("\\", "/");
+                    response.issuccess = true;
+                }
+                catch (Exception e)
+                {
+                    response.issuccess = false;
+                    response.message = "ERROR: " + tracking + " " + e.Message + ".";
+                }
             }
             string signal = JsonConvert.SerializeObject(response);
             return signal;
